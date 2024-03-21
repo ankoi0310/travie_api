666569ac58f24aa8195f02dbba5f76b9c4bafc81
koi <huynhvahuuan3620@gmail.com>
2024-03-22 00:51:09 +0700

    initial

---
+++ src/main/java/vn/edu/hcmuaf/fit/travie_api/TravieApiApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.travie_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravieApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravieApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/hcmuaf/fit/travie_api/TravieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.travie_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravieApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
