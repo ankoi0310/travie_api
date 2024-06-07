392c4f071576224cf4951e9d995dc350d4332bc0
koi <huynhvahuuan3620@gmail.com>
2024-06-07 21:39:14 +0700

    refactor: change response in controller

--- src/main/java/vn/edu/hcmuaf/fit/travie_api/controller/HotelController.java
+++ src/main/java/vn/edu/hcmuaf/fit/travie_api/controller/HotelController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.edu.hcmuaf.fit.travie_api.core.exception.BaseException;
+import vn.edu.hcmuaf.fit.travie_api.core.handler.HttpResponse;
 import vn.edu.hcmuaf.fit.travie_api.dto.hotel.*;
 import vn.edu.hcmuaf.fit.travie_api.service.HotelService;
 
@@ -16,26 +17,26 @@ public class HotelController {
     private final HotelService hotelService;
 
     @GetMapping("/search")
-    public ResponseEntity<List<HotelDTO>> search(HotelSearch hotelSearch) {
+    public ResponseEntity<HttpResponse> search(HotelSearch hotelSearch) {
         List<HotelDTO> hotels = hotelService.search(hotelSearch);
-        return ResponseEntity.ok(hotels);
+        return ResponseEntity.ok(HttpResponse.success(hotels, "Tìm kiếm thành công"));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<HotelDTO> getHotelById(@PathVariable long id) throws BaseException {
+    public ResponseEntity<HttpResponse> getHotelById(@PathVariable long id) throws BaseException {
         HotelDTO hotel = hotelService.getHotelById(id);
-        return ResponseEntity.ok(hotel);
+        return ResponseEntity.ok(HttpResponse.success(hotel, "Lấy thông tin khách sạn thành công"));
     }
 
     @PostMapping
-    public ResponseEntity<HotelDTO> createHotel(@RequestBody HotelCreate hotelCreate) throws BaseException {
+    public ResponseEntity<HttpResponse> createHotel(@RequestBody HotelCreate hotelCreate) throws BaseException {
         HotelDTO hotel = hotelService.createHotel(hotelCreate);
-        return ResponseEntity.ok(hotel);
+        return ResponseEntity.ok(HttpResponse.success(hotel, "Tạo khách sạn thành công"));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<HotelDTO> updateHotel(@PathVariable long id, @RequestBody HotelCreate hotelCreate) throws BaseException {
+    public ResponseEntity<HttpResponse> updateHotel(@PathVariable long id, @RequestBody HotelCreate hotelCreate) throws BaseException {
         HotelDTO hotel = hotelService.updateHotel(id, hotelCreate);
-        return ResponseEntity.ok(hotel);
+        return ResponseEntity.ok(HttpResponse.success(hotel, "Cập nhật khách sạn thành công"));
     }
 }
